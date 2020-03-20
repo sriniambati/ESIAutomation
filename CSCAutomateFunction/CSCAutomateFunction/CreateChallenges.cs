6e5b3fa5dbbb548ab512abf0a545159d4334ffe0
Srini Ambati <ambatis@gmail.com>
2020-03-20 15:09:48 -0400

    Added Http Trigger to the CreateChallenges Function App

--- CSCAutomateFunction/CSCAutomateFunction/CreateChallenges.cs
+++ CSCAutomateFunction/CSCAutomateFunction/CreateChallenges.cs
@@ -4,8 +4,15 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+<<<<<<< HEAD
 using System;
 using System.IO;
+=======
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+>>>>>>> Added Http Trigger to the CreateChallenges Function App
 using System.Threading.Tasks;
 
 
@@ -14,14 +21,36 @@ namespace CSCAutomateFunction
     public static class CreateChallenges
     {
         #region "Public Methods"
+<<<<<<< HEAD
         [FunctionName("CreateChallengesFromHttp")]
         public static async Task<IActionResult> RunHttp(
+=======
+        [FunctionName("CreateChallengesFromQueue")]
+        public static async Task Run(
+            [QueueTrigger("stq-cloudskillschallenge1")]string myQueueItem, 
+            ILogger log)
+        {
+            try
+            {
+                await CreateChallengesAsync(myQueueItem, log);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(string.Format("Error in CreateChallenges: {0}", ex.Message));
+                throw;
+            }
+        }
+
+        [FunctionName("CreateChallengesFromHttp")]
+        public static async Task<IActionResult> Run(
+>>>>>>> Added Http Trigger to the CreateChallenges Function App
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             try
             {
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+<<<<<<< HEAD
 
                 if (string.IsNullOrWhiteSpace(requestBody))
                 {
@@ -33,6 +62,11 @@ namespace CSCAutomateFunction
                 await CreateChallengesAsync(requestBody, log);
 
                 return new OkObjectResult(requestBody);
+=======
+                await CreateChallengesAsync(requestBody, log);
+                string responseMessage = "This HTTP triggered function executed successfully.";
+                return new OkObjectResult(responseMessage);
+>>>>>>> Added Http Trigger to the CreateChallenges Function App
             }
             catch (Exception ex)
             {
@@ -44,9 +78,20 @@ namespace CSCAutomateFunction
 
         #region "Private Methods"
 
+<<<<<<< HEAD
         private static async Task CreateChallengesAsync(string json, ILogger log)
         {
             log.LogInformation($"C# CreateChallenges function processing async: {json}");
+=======
+        static async Task CreateChallengesAsync([QueueTrigger("stq-cloudskillschallenge1")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# CreateChallenges function processing async: {myQueueItem}");
+            string jsonMessage = Regex.Unescape(myQueueItem);
+            string doubleQuote = "\"";
+            jsonMessage = jsonMessage.Remove(jsonMessage.IndexOf(doubleQuote), doubleQuote.Length);
+            jsonMessage = jsonMessage.Remove(jsonMessage.LastIndexOf(doubleQuote));
+
+>>>>>>> Added Http Trigger to the CreateChallenges Function App
             string environmentType = Environment.GetEnvironmentVariable("CSCAutomateEnvironment", EnvironmentVariableTarget.Process);
             string keyVaultName = Environment.GetEnvironmentVariable("CSCApiKeyVaultName", EnvironmentVariableTarget.Process);
 
@@ -54,9 +99,17 @@ namespace CSCAutomateFunction
             BlobApi blobApi = new BlobApi(config);
             CloudSkillApi cscApi = new CloudSkillApi(config);
 
+<<<<<<< HEAD
             await cscApi.CreateChallengesAsyc(blobApi, json);
 
             log.LogInformation($"C# Queue trigger function processed");
+=======
+            await cscApi.CreateChallengesAsyc(blobApi, jsonMessage);
+
+            //string jsonConfig = JsonConvert.SerializeObject(config);
+            //log.LogInformation($"Config: {jsonConfig}");
+            log.LogInformation($"C# Queue trigger function processed: {jsonMessage}");
+>>>>>>> Added Http Trigger to the CreateChallenges Function App
         }
 
         #endregion
